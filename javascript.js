cb86baef305ecf06bce6a5f7b2975c40d2e449bb
Thang <thanglehuu1510@gmail.com>
2017-09-22 21:02:40 +0700

    Initialize commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+function myFunction() {
+    alert(
+    document.getElementById("type-name").value+document.getElementById("bday").value + document.getElementById("opt").value + 
+    document.getElementById("radio").html + document.getElementById("checkbox").checked)
+ 
+    
+}
+
